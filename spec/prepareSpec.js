988fcbe1530e3fb901fcc160c7d4608a8ea2fd59
rikkertkoppes <rikkert.koppes@theapsgroup.nl>
2014-03-13 13:22:11 +0100

    fix out of order objects from returning its fields in the wrong order
    don't rely on the order of keys in the object. Instead, use the first object field order

--- spec/prepareSpec.js
+++ spec/prepareSpec.js
@@ -11,6 +11,19 @@ describe('prepare',function() {
                 var res = prep(arrayData);
                 expect(res.cols).toBeDefined();
             });
+            it('should create the correct cols',function() {
+                var res = prep(arrayData);
+                expect(res.cols).toEqual([{
+                    caption: 'name',
+                    type: 'string',
+                },{
+                    caption: 'date',
+                    type: 'string',
+                },{
+                    caption: 'number',
+                    type: 'number',
+                }]);
+            });
             it('should create caption and type field',function() {
                 var cols = prep(arrayData).cols;
                 expect(cols[0].caption).toBeDefined();
@@ -22,6 +35,12 @@ describe('prepare',function() {
             it('should create a rows part',function() {
                 var res = prep(arrayData);
                 expect(res.rows).toBeDefined();
+            });
+            it('should create rows with data in the correct order',function() {
+                var res = prep(arrayData);
+                expect(res.rows[0]).toEqual([ 'Ivy Dickson', '2013-05-27T11:04:15-07:00', 10 ]);
+                expect(res.rows[1]).toEqual([ 'Walker Lynch','2014-02-07T22:09:58-08:00',  2 ]);
+                expect(res.rows[2]).toEqual([ 'Maxwell U. Holden', '2013-06-16T05:29:13-07:00',  5]);
             });
         });
 
